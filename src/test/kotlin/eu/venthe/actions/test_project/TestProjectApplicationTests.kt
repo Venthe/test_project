ed6bf6a4d66cc1acd37577c05b4fa593c066d8cd
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-11-15 09:57:34 +0100

    Initial commit

---
+++ src/test/kotlin/eu/venthe/actions/test_project/TestProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.venthe.actions.test_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
